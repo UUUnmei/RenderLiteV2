58087937ee0e51ca85aa862863b08b4431a2fc00
Unmei <1551600819@qq.com>
2022-03-02 20:52:18 +0800

    clean

--- CubeScene.cpp
+++ CubeScene.cpp
@@ -22,25 +22,11 @@ void CubeScene::Init()
 	cam->SetDirection(1, 1, 1);
 	context->AddCamera( cam );
 	
-	//std::shared_ptr<Skybox> sky = std::make_shared<Skybox>("D:\\Visual Studio\\RenderLiteV2\\obj\\skybox\\default\\");
-	//context->AddSkybox( sky );
-
-	//context->AddUVSphere(10.0f).BindModelTex("obj/uvchecker/1/4096x4096 Texel Density Texture 1.png");
-	//context->AddModel("obj/nanosuit/nanosuit.obj");
-	//context->AddPlane(1000.0f)
-	//	.BindModelMat(glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)))
-	//	.BindModelTex("obj/uvchecker/1/4096x4096 Texel Density Texture 1.png");
 	context->AddModel("obj/Sponza/sponza.obj");
 	
 	drender.BindContext(context);
 	drender.GetShader().vs.BindMatProj(context->camera->GetProj());
 	drender.GetShader().vs.BindMatModel(context->models[0]->model_matrix);
-
-	PipelineConfig cfg;
-	cfg.fc_order = ConfigParams::FaceCullOrder::CW;
-	//skyrender.BindConfig(cfg);
-	//skyrender.BindContext(context);
-	//skyrender.GetShader().vs.BindMatProj(context->camera->GetProj());
 
 	context->SetRenderTarget(context->GetFrameBufferPointer());
 }
@@ -50,15 +36,10 @@ void CubeScene::Draw()
 	context->ClearBuffer();
 	context->camera_pos_cache = context->camera->GetCameraPosition();
 	context->camera_view_cache = context->camera->GetView();
-	
 
 	// draw pass
 	drender.GetShader().vs.BindMatView(context->camera_view_cache);
 	drender.Draw(0);
-
-	//skyrender.GetShader().vs.BindMatView(context->camera_view_cache);
-	//skyrender.DrawMesh(context->skybox->mesh);
-
 }
 
 void CubeScene::OnKeyChanged(int key, int scanCode, int action, int mode)
--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 
 #include <iostream>
 #include "Application.h"
-#include "PCSSScene.h"
 #include "CubeScene.h"
 
 
 int main()
 {
     
-    //Application(std::make_unique<PCSSScene>(800, 600)).Run();
     Application(std::make_unique<CubeScene>(800, 600)).Run();
 
     return 0;
